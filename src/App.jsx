e6be4a4df2a35518f04b1750afcd2e1f307f289c
danielemurgioni <dashomew@gmail.com>
2025-05-12 14:31:25 +0200

    [UPD] add film array

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const arrFilms = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 function App() {
 
